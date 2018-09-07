2111d7c8a817d8c8073ad1fa5a72b596324ecd99
Shingo Sato <shinsugar@gmail.com>
2018-09-07 17:27:10 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const { STATUS_CODES } = require('http')
+const { createError } = require('micro-errors')
+
+const parseAuthToken = auth => auth.trim().split('Bearer ')[1]
+
+module.exports = ({ authentication }) => {
+  if (typeof authentication !== 'function') {
+    throw new TypeError('`authentication` service client must be function')
+  }
+
+  return fn => async (req, res, ...rest) => {
+    const { authorization } = req.headers
+    if (!authorization) {
+      res.setHeader('WWW-Authenticate', 'Bearer realm=""')
+      throw createError(401, STATUS_CODES[401])
+    }
+
+    const idToken = parseAuthToken(authorization)
+    if (!idToken) {
+      res.setHeader('WWW-Authenticate', 'Bearer error="invalid_request"')
+      throw createError(400, STATUS_CODES[400], null, { detail: 'Invalid token format' })
+    }
+
+    const { uid: userId } = await authentication(idToken)
+    if (!userId) {
+      res.setHeader('WWW-Authenticate', 'Bearer error="invalid_token"')
+      throw createError(401, STATUS_CODES[401], null, { detail: 'Invalid token' })
+    }
+
+    req.userId = userId
+
+    return fn(req, res, ...rest)
+  }
+}
